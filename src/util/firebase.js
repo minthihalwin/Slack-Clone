521c32f67cec4174187246eec9be6dc8eeaa635d
minthihalwin <minthihalwin.dev@gmail.com>
2023-02-09 16:32:28 +1300

    first commit and created header component

---
+++ src/util/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBSFW_Sce17gFfdN3gwtfTwZ93aywefcEc",
+  authDomain: "slack-clone-33d79.firebaseapp.com",
+  projectId: "slack-clone-33d79",
+  storageBucket: "slack-clone-33d79.appspot.com",
+  messagingSenderId: "772132498913",
+  appId: "1:772132498913:web:db5f551c127f92afa036a0",
+};
